9d1a98b200d9e4a9e3c8df15137353e9ad39921b
luckykumardev <luckyu0020kumar@gmail.com>
2020-11-12 16:05:19 +0530

    Task management api

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import {TypeOrmModule}  from '@nestjs/typeorm';
 
 export  const typeOrmConfig : TypeOrmModule  ={
-    type: 'postgres',
-    host : 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'lucky@123',
-    database: 'taskmanagement',
+    type: '',
+    host : '',
+    port: '',
+    username: '',
+    password: '',
+    database: '',
     entities: [__dirname + '/../**/*.entity.js'],
     synchronize : true,
 }
